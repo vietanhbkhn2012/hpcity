0d509acc4216e584f4e8bafa61601c5fa993ecc9
vietanhbkhn2012 <vietanhbkhn2012@gmail.com>
2022-05-02 09:12:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
 "use strict";
-alert(`Cảm ơn bạn đã ghé thăm trang của mình! 😊`);
-let Name = prompt("Vui lòng nhập tên của bạn: ");
-alert(`Chúc bạn ${Name} có một ngày vui vẻ nhá 😊`);
+alert(`Cảm ơn bạn đã ghé thăm trang của mình! 😊\n Chúc bạn có một ngày vui vẻ nhá 🥰🥰🥰`);
